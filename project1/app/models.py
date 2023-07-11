9efb100561590f031617ddac9c6a5964b2364b87
Bogdanov499 <56233995+Bogdanov499@users.noreply.github.com>
2023-07-11 15:43:54 +0300

    models.py

--- project1/app/models.py
+++ project1/app/models.py
@@ -14,17 +14,6 @@ class User(Base):
 
     wallets = relationship("Wallet", back_populates="owner")
 
-
-#class Item(Base):
-#    __tablename__ = "items"
-#
-#    id = Column(Integer, primary_key=True, index=True)
-#    title = Column(String, index=True)
-#    description = Column(String, index=True)
-#    owner_id = Column(Integer, ForeignKey("users.id"))
-#
-#    owner = relationship("User", back_populates="items")
-#
 class Wallet(Base):
     __tablename__ = "wallets"
 
